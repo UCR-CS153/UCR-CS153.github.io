7d8ff692411d16bf59f1270fa0d75d4c324f49b3
Saber Ganjisaffar <s.ganjisaffar@gmail.com>
2024-10-02 17:53:15 -0700

    Update f24_lab0_autograder.py

--- f24_lab0_autograder.py
+++ f24_lab0_autograder.py
@@ -1,8 +1,3 @@
-import yaml
-import base64
-import re
-from pwn import *
-
 print(f"[!] All Checks Passed!")
 print(f"---> Your total score: 100 / 100")
 print(f"[!] You are now ready to begin working on Lab 1.")
