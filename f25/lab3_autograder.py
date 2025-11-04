f07e11cbd19b73b5fae7f7f0c37e7fe291152bc4
Muntashir Al-Islam <muntashirakon@riseup.net>
2025-11-03 20:29:19 -0800

    Add autograder for lab 3

---
+++ f25/lab3_autograder.py
@@ -0,0 +1,119 @@
+from pwn import *
+import yaml
+import base64
+import re
+from random import randint
+from os import system
+
+# test program with stack growth
+code = "I2luY2x1ZGUgInR5cGVzLmgiCiNpbmNsdWRlICJ1c2VyLmgiCgppbnQgdGVzdChpbnQgbikgeyAKICAgIGludCB4ID0gbiArIDE7CiAgICByZXR1cm4geDsKfQoKaW50IHRlc3QyKGludCBuKSB7CiAgICBpbnQgYVsxMDEzXSA9IHswfTsKICAgIHByaW50ZigxLCAiVGVzdCAyOiBhcnJheSBhIGlzIGF0ICVwXG4iLCBhKTsKICAgIC8vcHJpbnRmKDEsICJUZXN0IDI6IHRoZSBmaXJzdCB2YWx1ZSBpcyAlZFxuIiwgYVswXSk7CiAgICBpZihuPDIpCiAgICAgIHJldHVybiBuOwogICAgZWxzZQogICAgICByZXR1cm4gdGVzdDIobi0xKSsxOwp9CgppbnQgbWFpbihpbnQgYXJnYywgY2hhciAqYXJndltdKSB7CiAgICBpbnQgb3B0aW9uID0gYXRvaShhcmd2WzFdKTsKICAgIGludCBudW0gPSBhdG9pKGFyZ3ZbMl0pOwoKICAgIGlmIChvcHRpb24gPT0gMSl7CiAgICAgICAgaW50IGkgPSBhcmdjOwogICAgICAgIHByaW50ZigxLCAiQUREUjpfJXBfXG4iLCAmaSk7CiAgICAgICAgZXhpdCgpOwogICAgfQoKICAgIHByaW50ZigxLCAiVGVzdCAyOiBTdGFjayBncm93dGggdGVzdC5cbiIpOwogICAgCiAgICBwcmludGYoMSwgInRlc3QyOl8lZF9cbiIsIHRlc3QyKG51bSkpOwogICAgZXhpdCgpOwp9Cgo="
+def populate_makefile(filename):
+    c = open('Makefile', 'r').read().replace(" -Werror", " ")
+    uprogs = re.findall(r'UPROGS=([\w\W]*)fs\.img: mkfs', c)[0].replace("\\\n",'').split()
+    uprogs.insert(0, f'_{filename}')
+    uprogs = " ".join(uprogs)
+    c = re.sub(r'UPROGS=([\w\W]*)fs\.img: mkfs', f'UPROGS={uprogs} \nfs.img: mkfs', c)
+    open("Makefile", 'w').write(c)
+
+def post_to_gh(obtained, total):
+  """
+  Write points to for GitHub Actions
+  """
+  fd = os.environ.get('GITHUB_OUTPUT')
+  if fd is not None:
+    with open(fd, 'a') as out:
+        out.write(f'points={obtained}\n')
+        out.write(f'total_points={total}\n')
+  print(f"::notice title=Autograding complete::Points {obtained}/{total}")
+  print(f"::notice title=Autograding report::{{\"totalPoints\":{obtained},\"maxPoints\":{total}}}")
+
+code = base64.b64decode(code)
+full = 0
+
+populate_makefile("lab3_autograde")
+
+with open("lab3_autograde.c", 'wb') as f:   
+    f.write(code)
+
+system("make clean")
+
+p = process("make qemu-nox".split())
+
+points = 0
+
+try:
+    p.recvuntil(b"init: starting sh\n$", timeout=10)
+except:
+    print("[!]Failed to compile and start xv6 with testsuite")
+    print("[!]Compile log:", p.recvall().decode('latin-1'))
+    post_to_gh(0, 80)
+    exit(1)
+
+try:
+    addrs = {}
+    for _ in range(randint(50, 80)):
+        arg_count = randint(1, 7)
+        # Option 1 for testing new stack layout
+        cmd = "lab3_autograde 1 " + " ".join(map(str, range(arg_count)))
+        p.sendline(cmd.encode())
+
+        pids = []
+        stamps = []
+        line = p.recvline_regex(r"ADDR:_(.*)_".encode(), timeout=1).decode()
+        addr = re.findall(r"ADDR:_(.*)_", line)[0]
+        if int(addr, 16) < 0x7FFF0000:
+            raise Exception("")
+        if arg_count in addrs and addrs[arg_count] != addr:
+            raise Exception("")
+        else:
+            addrs[arg_count] = addr
+    print(cmd)
+    print(addrs)
+    keys = list(addrs.keys())
+    keys.sort()
+    last = 0x80000000
+    decending = False
+    for k in keys:
+        if int(addrs[k], 16) <= last:
+            _last = int(addrs[k], 16)
+            if _last != last:
+                decending = True
+            last = _last
+        else:
+            raise Exception("")
+    if not decending:
+        raise Exception("")
+    
+    points += 80
+    print("=======")
+    print(f"New stack layout passed.")
+    post_to_gh(points, 85)
+    
+    # Testing for stack growth
+    count = randint(50, 100)
+    cmd = f"lab3_autograde 2 {count}"
+    p.sendline(cmd.encode())
+
+    line = p.recvline_regex(r"test2:_(.*)_".encode(), timeout=1).decode()
+    ret = re.findall(r"test2:_(.*)_", line)[0]
+
+    if not int(ret) == count:
+        raise Exception("Stack growth failed")
+    
+    points += 5
+
+
+except Exception as e:
+    if points == 80:
+        print("[!]Stack growth test does not pass")
+        post_to_gh(points, 85)
+    else:
+        print(e)
+        print("[!]Verfiication does not pass")
+        post_to_gh(points, 85)
+        exit(1)
+    
+
+print("[!]All check finished!")
+print("=======")
+post_to_gh(points, 85)
