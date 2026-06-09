ac9083fc77834a1b1d40b1bf2f1e25b7c00fc9c5
Haochen <haochen.zeng10@gmail.com>
2026-06-09 16:54:19 -0700

    Update scoring system and test cases. Fix bonus test.

--- lab3.py
+++ lab3.py
@@ -5,10 +5,8 @@ import re
 from random import randint
 from os import system
 
-# code = "I2luY2x1ZGUgInR5cGVzLmgiCiNpbmNsdWRlICJzdGF0LmgiCiNpbmNsdWRlICJ1c2VyLmgiCmludCBtYWluKGludCBhcmdjLCBjaGFyICphcmd2W10pCnsKICBpbnQgaSA9IGFyZ2M7CiAgcHJpbnRmKDEsICJBRERSOl8lcF9cbiIsICZpKTsKICBleGl0KCk7Cn0K"
-
 # test program with stack growth
-code = "I2luY2x1ZGUgInR5cGVzLmgiCiNpbmNsdWRlICJ1c2VyLmgiCgppbnQgdGVzdChpbnQgbikgeyAKICAgIGludCB4ID0gbiArIDE7CiAgICByZXR1cm4geDsKfQoKaW50IHRlc3QyKGludCBuKSB7CiAgICBpbnQgYVsxMDEzXSA9IHswfTsKICAgIHByaW50ZigxLCAiVGVzdCAyOiBhcnJheSBhIGlzIGF0ICVwXG4iLCBhKTsKICAgIC8vcHJpbnRmKDEsICJUZXN0IDI6IHRoZSBmaXJzdCB2YWx1ZSBpcyAlZFxuIiwgYVswXSk7CiAgICBpZihuPDIpCiAgICAgIHJldHVybiBuOwogICAgZWxzZQogICAgICByZXR1cm4gdGVzdDIobi0xKSsxOwp9CgppbnQgbWFpbihpbnQgYXJnYywgY2hhciAqYXJndltdKSB7CiAgICBpbnQgb3B0aW9uID0gYXRvaShhcmd2WzFdKTsKICAgIGludCBudW0gPSBhdG9pKGFyZ3ZbMl0pOwoKICAgIGlmIChvcHRpb24gPT0gMSl7CiAgICAgICAgaW50IGkgPSBhcmdjOwogICAgICAgIHByaW50ZigxLCAiQUREUjpfJXBfXG4iLCAmaSk7CiAgICAgICAgZXhpdCgpOwogICAgfQoKICAgIHByaW50ZigxLCAiVGVzdCAyOiBTdGFjayBncm93dGggdGVzdC5cbiIpOwogICAgCiAgICBwcmludGYoMSwgInRlc3QyOl8lZF9cbiIsIHRlc3QyKG51bSkpOwogICAgZXhpdCgpOwp9Cgo="
+code = "I2luY2x1ZGUgInR5cGVzLmgiCiNpbmNsdWRlICJwYXJhbS5oIgojaW5jbHVkZSAidXNlci5oIgoKaW50CmJhc2ljX3Rlc3Qodm9pZCkKewogIGludCBsb2NhbDsKICB1aW50IGFkZHIgPSAodWludCkmbG9jYWw7CgogIHByaW50ZigxLCAic3RhY2tncm93OiBiYXNpYyBzdGFjayBhZGRyZXNzICVwXG4iLCAmbG9jYWwpOwoKICAvLyBGb3IgdGhlIG5ldyB0b3Atb2YtdXNlci1zcGFjZSBzdGFjayBsYXlvdXQsIHRoZSBzdGFjayBzaG91bGQgYmUKICAvLyB3aXRoaW4gYSBoaWdoIHVzZXItc3BhY2UgcmFuZ2UganVzdCBiZWxvdyBLRVJOQkFTRS4KICBpZihhZGRyIDwgMHg3ZjAwMDAwMHUgfHwgYWRkciA+IDB4N2ZmZmZmZmZ1KQogICAgcmV0dXJuIDA7CiAgcmV0dXJuIDE7Cn0KCmludApib251c190ZXN0KGludCBkZXB0aCkKewogIHZvbGF0aWxlIGNoYXIgYnVmWzQwOTZdOwogIGludCBpOwoKICBmb3IoaSA9IDA7IGkgPCBzaXplb2YoYnVmKTsgaSArPSA0MDk2KQogICAgYnVmW2ldID0gZGVwdGg7CgogIGlmKGRlcHRoIDw9IDApCiAgICByZXR1cm4gMDsKCiAgcmV0dXJuIGJvbnVzX3Rlc3QoZGVwdGggLSAxKSArIDE7Cn0KCmludAptYWluKGludCBhcmdjLCBjaGFyICphcmd2W10pCnsKICBpbnQgc3VjY2VzczsKCiAgcHJpbnRmKDEsICJzdGFja2dyb3c6IHJ1bm5pbmcgYmFzaWMgc3RhY2sgbGF5b3V0IHRlc3RcbiIpOwogIHN1Y2Nlc3MgPSBiYXNpY190ZXN0KCk7CiAgaWYoc3VjY2VzcykKICAgIHByaW50ZigxLCAic3RhY2tncm93OiBiYXNpYyB0ZXN0IHN1Y2Nlc3NcbiIpOwogIGVsc2UgewogICAgcHJpbnRmKDEsICJzdGFja2dyb3c6IGJhc2ljIHRlc3QgZmFpbHVyZVxuIik7CiAgICBleGl0KCk7CiAgfQoKICBwcmludGYoMSwgInN0YWNrZ3JvdzogcnVubmluZyBib251cyBzdGFjayBncm93dGggdGVzdFxuIik7CiAgc3VjY2VzcyA9IGJvbnVzX3Rlc3QoOCk7CiAgaWYoc3VjY2VzcyA9PSA4KQogICAgcHJpbnRmKDEsICJzdGFja2dyb3c6IGJvbnVzIHRlc3Qgc3VjY2VzcyAoZGVwdGg9OClcbiIpOwogIGVsc2UKICAgIHByaW50ZigxLCAic3RhY2tncm93OiBib251cyB0ZXN0IGZhaWx1cmVcbiIpOwoKICBleGl0KCk7Cn0K"
 def populate_makefile(filename):
     c = open('Makefile', 'r').read().replace(" -Werror", " ")
     uprogs = re.findall(r'UPROGS=([\w\W]*)fs\.img: mkfs', c)[0].replace("\\\n",'').split()
@@ -36,73 +34,37 @@ try:
 except:
     print("[!]Failed to compile and start xv6 with testsuite")
     print("[!]Compile log:", p.recvall().decode('latin-1'))
-    print(f"Your score: 0 / 80")
+    print(f"Your score: 0 / 90")
     exit(1)
 
 try:
-    addrs = {}
-    for _ in range(randint(50, 80)):
-        arg_count = randint(1, 7)
-        # Option 1 for testing new stack layout
-        cmd = "lab3_autograde 1 " + " ".join(map(str, range(arg_count)))
-        p.sendline(cmd.encode())
+    p.sendline(b"lab3_autograde")
 
-        pids = []
-        stamps = []
-        line = p.recvline_regex(r"ADDR:_(.*)_".encode(), timeout=1).decode()
-        addr = re.findall(r"ADDR:_(.*)_", line)[0]
-        if int(addr, 16) < 0x7FFF0000:
-            raise Exception("")
-        if arg_count in addrs and addrs[arg_count] != addr:
-            raise Exception("")
-        else:
-            addrs[arg_count] = addr
-    print(cmd)
-    print(addrs)
-    keys = list(addrs.keys())
-    keys.sort()
-    last = 0x80000000
-    decending = False
-    for k in keys:
-        if int(addrs[k], 16) <= last:
-            _last = int(addrs[k], 16)
-            if _last != last:
-                decending = True
-            last = _last
-        else:
-            raise Exception("")
-    if not decending:
-        raise Exception("")
-    
-    points += 75
+    line = p.recvline_regex(r"stackgrow: basic stack address (.*)".encode(), timeout=5).decode()
+    addr = re.findall(r"stackgrow: basic stack address (.*)", line)[0]
+    if int(addr, 16) < 0x7F000000 or int(addr, 16) > 0x7FFFFFFF:
+        raise Exception("Stack address out of range")
+
+    p.recvline_regex(r"stackgrow: basic test success".encode(), timeout=5)
+    points += 80
     print("=======")
-    print(f"New stack layout passed. Your score: {points} / 80")
-    
-    # Testing for stack growth
-    count = randint(50, 100)
-    cmd = f"lab3_autograde 2 {count}"
-    p.sendline(cmd.encode())
+    print(f"New stack layout passed. Your score: {points} / 90")
 
-    line = p.recvline_regex(r"test2:_(.*)_".encode(), timeout=1).decode()
-    ret = re.findall(r"test2:_(.*)_", line)[0]
-
-    if not int(ret) == count:
-        raise Exception("Stack growth failed")
-    
-    points += 5
-
+    p.recvline_regex(r"stackgrow: running bonus stack growth test".encode(), timeout=5)
+    p.recvline_regex(r"stackgrow: bonus test success \(depth=8\)".encode(), timeout=5)
+    points += 10
 
 except Exception as e:
-    if points == 75:
+    if points == 80:
         print("[!]Stack growth test does not pass")
-        print(f"Your score: {points} / 80")
+        print(f"Your score: {points} / 90")
     else:
         print(e)
         print("[!]Verfiication does not pass")
-        print(f"Your score: {points} / 80")
+        print(f"Your score: {points} / 90")
         exit(1)
     
 
 print("[!]All check finished!")
 print("=======")
-print(f"Your score: {points} / 80")
+print(f"Your score: {points} / 90")
